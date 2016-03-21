fd4ebd70dcf1bfea5df958ccbf1ee27a5489427f
Anthony Gold and Nick Arrasate <anthony@>
2016-03-21 14:22:26 -0700

    Adds JQuery funcionality to Headers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
 
 
-
-/*==== Lbs to Kgs
-========*/
-var lbToKg = function(lb) {
-	return lb * 2.2
-}
-var lb = parseInt(prompt("How many pounds to convert to kilos?"))
-var kiloResult = lbToKg(lb);
-alert(kiloResult.toPrecision(4) + "Kgs")
-
-/*==== Gallons to Liters
-========*/
-var gallonToLiter = function(gallon) {
-	return gallon / .26417
-}
-var gallon = parseInt(prompt("How many gallons to convert?"))
-var literResult = gallonToLiter(gallon);
-alert(literResult.toPrecision(4) + "L"))
-
-/*==== Cups to Tbsp
-========*/
-var cupsToTbsp = function(cup) {
-	return cup / .26417
-}
-var cup = parseInt(prompt("How many cups to convert?"))
-var tbspResult = cupsToTbsp(cup);
-alert(tbspResult.toPrecision(4) + "L"))
+$(document).ready(function() {
+  /*==== Lbs to Kgs
+  ========*/
+  $(".kilos").click(function (){
+    var lbToKg = function(lb) {
+    return lb * 2.2;
+    };
+    var lb = parseInt(prompt("How many pounds to convert to kilos?"));
+    var kiloResult = lbToKg(lb);
+    alert(kiloResult.toPrecision(4) + "Kgs");
+});
+  /*==== Gallons to Liters
+  ========*/
+  $(".liters").click(function(){
+    var gallonToLiter = function(gallon) {
+    return gallon / .26417;
+    };
+    var gallon = parseInt(prompt("How many gallons to convert?"));
+    var literResult = gallonToLiter(gallon);
+    alert(literResult.toPrecision(4) + "L");
+});
+  /*==== Cups to Tbsp
+  ========*/
+  $(".cups").click(function() {
+    var cupsToTbsp = function(cup) {
+  	return cup * 16;
+    };
+    var cup = parseInt(prompt("How many cups to convert?"));
+    var tbspResult = cupsToTbsp(cup);
+    alert(tbspResult.toPrecision(4) + "Tbsp");
+  });
+});
